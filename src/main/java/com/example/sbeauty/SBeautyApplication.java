21bf27b34609580c4d19755b31e7383287a362d8
manhpdhe140831 <manhpdhe140831@fpt.edu.vn>
2022-09-23 17:37:13 +0700

    Source Code

---
+++ src/main/java/com/example/sbeauty/SBeautyApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBeautyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SBeautyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbeauty/SBeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBeautyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
